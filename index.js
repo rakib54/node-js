5abbb035fad2d421d5bc3cb7431c27a4f4a79011
Rakibur Rahman <rakibur5454@gmail.com>
2021-03-23 22:08:01 +0600

    added postman

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+
 
 
 const app = express();
 
-app.get('/',(req, res)=>{
-    res.send('Thanks for calling me')
+app.use(cors());
+app.use(bodyParser.json())
+
+
+const users = ['Rakib', 'Tamim', 'fahim', 'shakib'];
+
+app.get('/', (req, res) => {
+    const profile = {
+        name: 'Rakib',
+        age: 23,
+        salary: 20000
+
+    }
+    res.send(profile)
+});
+
+app.get('/profile/:id', (req, res) => {
+    const UserId = req.params.id;
+    console.log(req.query)
+    const name = users[UserId]
+    res.send({ UserId, name })
+})
+// post
+app.post('/addUser', (req, res) => {
+
+    const user = req.body;
+    user.id = 55
+    res.send(user)
 })
 
-app.listen(3000, ()=>console.log('listening from 3000'));
+app.listen(3000, () => console.log('listening from 3000'));
